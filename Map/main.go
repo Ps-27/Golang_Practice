53792065adf762e3c1bae8ee2a364769e7792d9c
Prity Sinha <71940538+Ps-27@users.noreply.github.com>
2023-08-21 14:55:19 +0530

    Add files via upload

--- Map/main.go
+++ Map/main.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func main(){
+
+	// var colors map[string]string
+    // colors:=make(map[int]string)
+	colors := map[string]string{
+		"red":"#ff0000",
+		"green":"#4bf745",
+		"white":"#28f444",
+
+	}
+
+	// colors[10]="#wo97"
+	// delete(colors,10)
+	for i,v:=range colors{
+		fmt.Printf("key %s and value %v\n", i, v)
+	}
+	
+	fmt.Println(colors)
+}
